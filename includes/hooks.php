b088b6d6a136f29352d8391c1618fc5afcf5dc63
Maxime BERNARD-JACQUET <maxime@dysign.fr>
2020-12-26 12:55:55 +0100

    Improved the way summary is generated

--- includes/hooks.php
+++ includes/hooks.php
@@ -27,40 +27,55 @@ function capitaine_table_of_contents( $post_id, $post, $update )  {
   if( wp_is_post_revision( $post_id ) ) { return; }
   if( defined( 'DOING_AUTOSAVE' ) and DOING_AUTOSAVE ) { return; }
   if( $post->post_type != 'post' ) { return; }
-	
-  require_once get_template_directory() . '/lib/simple_html_dom.php';
-  $html = str_get_html( $post->post_content );
-    
-  // On initialise le html du sommaire
-	$summary = "<ul class='summary'>";
 
-  foreach( $html->find( 'h2, h3, h4' ) as $element ){
+  // Étape 1 : création des ancres dans le contenu
 
-    $title = $element->innertext;
-    if ( $title == "" ) { continue; }
+  // Recherche des titres dans le contenu
+  $content = preg_replace_callback( 
+    "/<h([2-4])(.*?)>(.*?)<\/h([2-4])>/i", // L'expression régulière
+    function( $matches ) { // La fonction de remplacement
+      $level = $matches[1];
+      $slug = sanitize_title( $matches[3] );
+      $title = $matches[3];
 
-    // Mon titre deviendra alors mon-titre
-    $slug = sanitize_title( $title );
-
-    // On ajoute id="mon-titre" à chaque titre
-    $element->id = $slug;
-  
-    // On ajoute une entrée au sommaire
-    $summary .= "<li><a href='#$slug'>$title</a></li>";
-
-  }
+      return "<h$level id='$slug'>$title</h$level>";
+    }, 
+    $post->post_content
+  );
 
   // Pour éviter une boucle infinie, on désactive le hook
   remove_action( 'save_post', 'capitaine_table_of_contents', 10, 3 );
 
   // On met à jour le HTML de l'article
-  wp_update_post( array( 'ID' => $post_id, 'post_content' => $html ) );
+  wp_update_post( array( 'ID' => $post_id, 'post_content' => $content ) );
 
   // Réactiver le hook
   add_action( 'save_post', 'capitaine_table_of_contents', 10, 3 );
+
+
+  // Étape 2 : génération du sommaire
+
+  // On initialise le html du sommaire
+  $summary = "<ul class='summary'>";
+
+  // On récupère
+  preg_match_all(
+    "/<h([2-4])(.*?)>(.*?)<\/h([2-4])>/i",
+    $post->post_content,
+    $matches,
+    PREG_SET_ORDER
+  );
+
+  // On ajoute une entrée au sommaire
+  foreach( $matches as $match ) {
+    $slug = sanitize_title( $match[3] );
+    $title = $match[3];
     
+    $summary .= "<li><a href='#$slug'>$title</a></li>";
+  }
+  
   // On referme le sommaire
-	$summary .= "</ul>";
+  $summary .= "</ul>";
 
 	// Et on l'enregistre dans une meta liée à l'article
 	update_post_meta( $post_id, 'summary', $summary );
