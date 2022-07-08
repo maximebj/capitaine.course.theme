d893e71173d685d6bfb35c18f4534eb0c9410938
Maxime BERNARD-JACQUET <maxime@dysign.fr>
2022-07-08 18:44:24 +0200

    Added http code 403 error to wp_send_json_error

--- includes/ajax.php
+++ includes/ajax.php
@@ -7,12 +7,12 @@ function capitaine_load_comments() {
   
   // Vérification de sécurité
   if( ! isset( $_REQUEST['nonce'] ) or ! wp_verify_nonce( $_REQUEST['nonce'], 'capitaine_load_comments' ) ) {
-    wp_send_json_error( "Vous n’avez pas l’autorisation d’effectuer cette action." );
+    wp_send_json_error( "Vous n’avez pas l’autorisation d’effectuer cette action.", 403 );
   }
 
   // On vérifie que l'identifiant a bien été envoyé
   if( ! isset( $_POST['postid'] ) ) {
-    wp_send_json_error( "L'identifiant de l'article est manquant." );
+    wp_send_json_error( "L'identifiant de l'article est manquant.", 403 );
   }
 
   // Récupération des données du formulaire
@@ -20,7 +20,7 @@ function capitaine_load_comments() {
 
   // Vérifier que l'article est publié, et public
   if( get_post_status( $post_id ) !== 'publish' ) {
-    wp_send_json_error( "Vous ne pouvez pas accéder aux commentaires de cet article." );
+    wp_send_json_error( "Vous ne pouvez pas accéder aux commentaires de cet article.", 403 );
   }
 
   // Utilisez sanitize_text_field() pour une chaine de caractères.
